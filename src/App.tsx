755ce57f3e334f0eea5089bb48e32864cd0185ab
Asher9889 <saurabhkushwaha9889@gmail.com>
2025-08-03 19:46:18 +0530

    Folder Structure Complete

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Home from './pages/home/Home'
+
+function App() {
+
+  return (
+    <div className='bg-red-500'>
+      <Home />
+    </div>
+  )
+}
+
+export default App
