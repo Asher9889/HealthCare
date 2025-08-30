688a060e85eb51d20b01266a2f3359cf1a0540d1
Saurabh <saurabh@Saurabhs-MacBook-Air.local>
2025-08-30 17:13:32 +0530

    .

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -33,7 +33,7 @@ const Sidebar = () => {
           <h3 id="hs-offcanvas-left-label" className="font-bold text-white">
             Menu
           </h3>
-          <button type="button" className="size-8 inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent bg-white text-gray-800 hover:bg-gray-200 focus:outline-hidden focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none" aria-label="Close" data-hs-overlay="#hs-offcanvas-left">
+          <button type="button" className="m-1 ms-0 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium  border border-transparent focus:outline-hidden  disabled:opacity-50 disabled:pointer-events-none" aria-haspopup="dialog" aria-expanded="false" aria-controls="hs-offcanvas-left" data-hs-overlay="#hs-offcanvas-left">
             <span className="sr-only">Close</span>
             <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
               <path d="M18 6 6 18"></path>
