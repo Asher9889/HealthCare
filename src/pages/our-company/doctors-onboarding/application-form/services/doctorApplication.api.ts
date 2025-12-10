c529cfbba4c1845c4078db2f1e9979a26f57fbdb
Saurabh Kushwaha <saurabhkushwaha9889@gmail.com>
2025-12-10 21:48:32 +0530

    Doctor onboarding issue resolved

--- src/pages/our-company/doctors-onboarding/application-form/services/doctorApplication.api.ts
+++ src/pages/our-company/doctors-onboarding/application-form/services/doctorApplication.api.ts
@@ -3,10 +3,15 @@ import type { TDoctorApplicationFormValues } from "../schema/application.schema"
 
 export const doctorApplicationApi = {
     submit: async (data: TDoctorApplicationFormValues) => {
+        const changedData = {
+            ...data,
+            expMbbs: parseInt(data.expMbbs),
+            expPg: data.expPg?.trim() ? parseInt(data.expPg) : null,
+        }
         const res = await axiosInstance.request({
             url: endPoints.onboardingDoctor.url(),
             method: endPoints.onboardingDoctor.method,
-            data
+            data: changedData
         })
 
         return res.data;
